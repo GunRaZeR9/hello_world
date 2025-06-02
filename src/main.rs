28487b08f742ce2d5f2c07efdcd94b1a50770db4
GunRaZeR9 <paul.tatar11@gmail.com>
2025-06-02 12:49:42 +0300

    Project init Course 1 finished. Course 2 - declaring variables

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// This is a simple Rust program that prints "Hello, world!" to the console.
+// You can run this program using `cargo run` in the terminal.
+// Make sure you have Rust installed and set up correctly.
+fn main() {
+    
+    let x = 10;
+
+    println!("x is a{}", x);
+
+}
